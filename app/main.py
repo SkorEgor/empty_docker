3c49d0414f5ad0e030fc7e4a2bae9f613199b526
SkorEgor <e2622001@gmail.com>
2024-11-25 19:16:28 +0300

    refactoring

--- app/main.py
+++ app/main.py
@@ -1,16 +1,23 @@
 from starlette.applications import Starlette
-from starlette.responses import JSONResponse
+from starlette.requests import Request
+from starlette.responses import JSONResponse, HTMLResponse
 from starlette.routing import Route
 
 
-async def greet_user(request):
-    data = await request.json()
-    name = data.get("name", "гость")
-    return JSONResponse({"message": f"Привет!, {name}!"})
+async def greet_user(request: Request) -> JSONResponse:
+    name = (await request.json()).get("name", "гость")
+    return JSONResponse({"message": f"Привет, {name}!"})
 
 
-app = Starlette(
-    routes=[
-        Route("/greet", greet_user, methods=["POST"]),
-    ]
-)
+async def list_routes(request: Request) -> HTMLResponse:
+    routes_html = "<ul>"
+    for route in app.routes:
+        methods = ", ".join(route.methods)
+        routes_html += f'<li><strong>{route.path}</strong> - [{methods}]</li>'
+    routes_html += "</ul>"
+    return HTMLResponse(f"<h1>Список маршрутов</h1>{routes_html}")
+
+app = Starlette(routes=[
+    Route("/", list_routes),
+    Route("/greet", greet_user, methods=["POST"]),
+])
